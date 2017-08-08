4d7c768e788fff8e2dd989fd349d30143c9db571
zhouzheng <zhouzheng@pool-100-2-7-240.nycmny.fios.verizon.net>
2017-08-08 09:19:05 +0800

    delete surplus code

--- ZZTouchID/AppDelegate.m
+++ ZZTouchID/AppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,12 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
-//    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-//    self.window.backgroundColor = [UIColor whiteColor];
-//    ViewController *vc = [[ViewController alloc] init];
-//    self.window.rootViewController = vc;
-    
     return YES;
 }
 
